e1cf8d77395e7aa3adc7bf1de9dafe4f4dc74f23
winter jung <me@winterjung.dev>
2021-03-02 10:06:55 +0900

    Remove language specific installations

--- brew.sh
+++ brew.sh
@@ -17,36 +17,14 @@ brew install fd
 brew install the_silver_searcher
 brew install fzf
 brew install z
-brew install htop
 brew install httpie
-brew install icdiff
 brew install jq
 brew install tldr
 brew install ncdu
 brew install hexyl
-brew install supervisor
-brew install ngrok
+brew install asdf
+brew install bat
 
-# Just for fun
-brew install lolcat
-
-# Python
-brew install pyenv
-brew install pipenv
-brew install zlib
-CFLAGS="-I$(brew --prefix openssl)/include -I$(xcrun --show-sdk-path)/usr/include" LDFLAGS="-L$(brew --prefix openssl)/lib" pyenv install 3.7.4
-
-# Go
-brew install go
-brew install gomplate
-
-# Node
-brew install nvm
-
-# DevOps
-brew cask install vagrant
-brew install ansible
-brew install packer
-brew install terraform
-brew install kubernetes-helm
-
+# TODO
+# (languages) Go, Python, Node, (ops) Terraform, Kubernetes
+brew install kubectl
